c814250e23d1e19b327c42852e7334d1538953be
luxiag <luxiag@qq.com>
2024-11-12 15:15:22 +0800

    feat: 拖拽框拖动

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-cropper
+# vue-ts-cropper
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/components/ImgCropper.vue
+++ src/components/ImgCropper.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="image-cropper" ref="imageCropperRef" @mouseover="handleMouseOver" @mouseout="handleMouseOut">
+  <div class="image-cropper" ref="imageCropperRef">
 
-      <div class="img-cropper__canvas">
-        <img :src="cropperModel.url" alt="" />
-      </div>
-      <div class="img-cropper__cut" :style="{
-        'width': cutModel.width + 'px',
-        'height': cutModel.height + 'px',
-        'top': cutModel.y + 'px',
-        'left': cutModel.x + 'px'
-      }">
+    <div class="img-cropper__canvas">
+      <img :src="cropperModel.url" alt="" />
+    </div>
+    <div class="img-cropper__cut"       :style="{
+      'width': cutModel.width + 'px',
+      'height': cutModel.height + 'px',
+      'transform':`translate(${cutModel.x}px, ${cutModel.y}px)`
+
+    }" 
+     @mousedown="handleCutModelMoveDownEvent" 
+     @touchstart="handleCutModelMoveDownEvent"
+    >
+
       <ul class="img-cropper__cut__handle">
         <li class="img-cropper__cut__left_top square_blue"></li>
-        <li class="img-cropper__cut__top square_blue"></li>
+        <li class="img-cropper__cut__top square_blue" ></li>
         <li class="img-cropper__cut__right_top square_blue"></li>
         <li class="img-cropper__cut__left square_blue"></li>
         <li class="img-cropper__cut__right square_blue"></li>
         <li class="img-cropper__cut__left_bottom square_blue"></li>
-        <li class="img-cropper__cut__bottom"></li>
+        <li class="img-cropper__cut__bottom square_blue"></li>
         <li class="img-cropper__cut__right_bottom square_blue"></li>
 
         <li class="img-cropper__cut__line_top"></li>
@@ -26,66 +30,192 @@
         <li class="img-cropper__cut__line_left"></li>
       </ul>
 
-      </div>
-      <div class="img-cropper__drop"></div>
-      <div class="img-cropper__modal"></div>
- 
-  
+    </div>
+    <div class="img-cropper__drop"></div>
+    <div class="img-cropper__modal"></div>
+
+
   </div>
 
 </template>
 <script setup lang="ts">
-import { ref,reactive,watch } from 'vue'
+import { ref, reactive, watch, onMounted, nextTick } from 'vue'
 
 type ImgType = String | Blob | null | File
 
-const imageCropperRef = ref(null)
+const imageCropperRef = ref<HTMLElement>()
 
 const props = defineProps({
   img: {
-    type: [String,Blob,null,File],
+    type: [String, Blob, null, File],
     default: ''
   },
   canvasHeight: {
     type: [Number],
-    default:0
+    default: 0
   },
   canvasWidth: {
     type: [Number],
-    default:0
+    default: 0
   }
 })
 
 const cropperModel = reactive({
   img: '',
-  url:'',
+  url: '',
   loading: false,
   scale: 1,
   rotate: 0,
-  orientation:1
+  orientation: 1
 
 })
+
+
+// 剪切框
+
+enum MoveTypeEnum {
+  left_top = 'left_top',
+  top = 'top',
+  right_top = 'right_top',
+  left = 'left',
+  right = 'right',
+  left_bottom = 'left_bottom',
+  bottom = 'bottom',
+  right_bottom = 'right_bottom'
+}
 
 const cutModel = reactive({
-  width:0,
-  height:0,
-  x:0,
-  y:0
+  maxWidth: 0,
+  maxHeight: 0,
+  width: 0,
+  height: 0,
+  x: 0,
+  y: 0,
+  // start move point
+  startMoveX:0,
+  startMoveY:0,
+  beforeMoveX:0,
+  beforeMoveY:0,
+  moveType: MoveTypeEnum.left_top
 })
 
 
 
+
+type MouseEventType = MouseEvent | TouchEvent
+
+const handleCutModelMoveDownEvent = (e: MouseEventType) => {
+  e.preventDefault()
+  // 双指触控
+  if('touches' in e&&e.touches.length>1){ 
+    return
+  }
+
+
+  window.addEventListener('mousemove', moveCutModelEvent)
+  window.addEventListener('mouseup',leaveCutModelEvent)
+  window.addEventListener('touchmove', moveCutModelEvent)
+  window.addEventListener('touchend', leaveCutModelEvent)
+
+  cutModel.startMoveX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
+  cutModel.startMoveY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
+
+  cutModel.beforeMoveX = cutModel.x
+  cutModel.beforeMoveY = cutModel.y
+  
+}
+
+const moveCutModelEvent = (e: MouseEventType) => {
+ e.preventDefault()
+
+
+  const movingX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
+  const movingY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
+
+  const lengthX =movingX - cutModel.startMoveX + cutModel.beforeMoveX
+  const lengthY =movingY - cutModel.startMoveY + cutModel.beforeMoveY
+  nextTick(()=>{
+    cutModel.x = lengthX < 0 ? 0 : lengthX > cutModel.maxWidth - cutModel.width ? cutModel.maxWidth - cutModel.width : lengthX;
+    cutModel.y = lengthY < 0 ? 0 : lengthY > cutModel.maxHeight - cutModel.height ? cutModel.maxHeight - cutModel.height : lengthY;
+  })
+
+  
+}
+
+const leaveCutModelEvent = (e:MouseEventType) => {
+  window.removeEventListener('mousemove', moveCutModelEvent)
+  window.removeEventListener('mouseup', leaveCutModelEvent)
+  window.removeEventListener('touchmove', moveCutModelEvent)
+  window.removeEventListener('touchend', leaveCutModelEvent)
+}
+
+
+const handleCutModelSizeEvent = (e:MouseEventType,type:MoveTypeEnum ) => {
+  e.preventDefault()
+  cutModel.startMoveX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
+  cutModel.startMoveY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
+  window.addEventListener('mousemove',changeCutModelSize)
+  window.addEventListener('mouseup',leaveCutModelSize)
+  window.addEventListener('touchmove',changeCutModelSize)
+  window.addEventListener('touchend',leaveCutModelSize)
+  cutModel.moveType = type
+
+
+}
+
+const changeCutModelSize = (e: MouseEventType) => {
+  e.preventDefault()
+
+  const movingX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
+  const movingY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
+
+  switch(cutModel.moveType) { 
+    case MoveTypeEnum.top:
+      const height = cutModel.startMoveY - movingY 
+      cutModel.height = cutModel.height + height > cutModel.maxHeight ? cutModel.maxHeight : cutModel.height + height < 0 ? 0 : cutModel.height + height
+      cutModel.y = cutModel.y - height < 0 ? 0 : cutModel.y - height > cutModel.maxHeight ? cutModel.maxHeight: cutModel.y - height
+    case MoveTypeEnum.bottom:
+    case MoveTypeEnum.left:
+    case MoveTypeEnum.right:
+
+  }
+
+
+}
+const leaveCutModelSize = (e:MouseEventType) => {
+
+  window.removeEventListener('mousemove', changeCutModelSize)
+  window.removeEventListener('mouseup', leaveCutModelSize)
+  window.removeEventListener('touchmove', changeCutModelSize)
+  window.removeEventListener('touchend', leaveCutModelSize)
+}
+
+
+
+const initCutModel = () => {
+  const width = imageCropperRef.value?.offsetWidth || 500
+  cutModel.maxWidth = width
+  cutModel.maxHeight = width
+  cutModel.width = width -200
+  cutModel.height = width -200
+  cutModel.x = 0
+  cutModel.y = 0
+}
+
+onMounted(()=>{
+  initCutModel()
+})
 
 const handleImgChange = () => {
 
-  
+
   if (!props.img) {
     handleClearCropper()
     return
 
   }
 
-  console.log('img changed',props.img)
+  console.log('img changed', props.img)
   cropperModel.loading = true
   cropperModel.scale = 1
   cropperModel.rotate = 0
@@ -97,11 +227,11 @@ const handleImgChange = () => {
     let height = img.height
     // const imageData = await getImageData(img)
     // cropperModel.orientation = imageData.orientation 
-    const maxWidth = imageCropperRef.value.offsetWidth
-    console.log(maxWidth,'maxWidth')
-    if(width > maxWidth) {
-      const rate = height /width 
-      width = maxWidth 
+    const maxWidth = imageCropperRef.value?.offsetWidth || 0
+    console.log(maxWidth, 'maxWidth')
+    if (width > maxWidth && maxWidth !== undefined) {
+      const rate = height / width
+      width = maxWidth
       height = rate * width
     }
 
@@ -115,15 +245,15 @@ const handleImgChange = () => {
 
 
   img.onerror = (error) => {
-    console.log('onerror',error)
+    console.log('onerror', error)
     cropperModel.loading = false
   }
 
 
 
-  if(props.img instanceof File ) {
+  if (props.img instanceof File) {
 
-    if(!props.img.type.indexOf('image') === 0) { 
+    if (props.img.type.indexOf('image') !== 0) {
       console.log('not image')
       return
     }
@@ -132,22 +262,28 @@ const handleImgChange = () => {
 
       // img.src = reader.result
       const arrayBuffer = reader.result
-      const blob = new Blob([arrayBuffer],{type: props.img?.type || 'image/png'})
-      const URL = window.URL || window.webkitURL
-      const src = URL.createObjectURL(blob)
-      img.src = src
+      if (!arrayBuffer) return
+      if (props.img instanceof File) {
+        const blob = new Blob([arrayBuffer], { type: props.img.type || 'image/png' })
+        const URL = window.URL || window.webkitURL
+        const src = URL.createObjectURL(blob)
+        img.src = src
+      }else {
+        console.error('props.img is not File')
+      }
+
 
 
     }
     reader.onerror = (error) => {
-      console.error('FileReader',error)
+      console.error('FileReader', error)
     }
     // // 转换为base64
     // // render.readAsDataURL(props.img)
 
     // 转换为arrayBuffer
     reader.readAsArrayBuffer(props.img)
-  }else {
+  } else if (typeof props.img === 'string') {
     img.src = props.img
   }
 
@@ -156,19 +292,24 @@ const handleImgChange = () => {
 }
 watch(() => props.img, handleImgChange)
 
-const drawImageToCanvas = (img:Image,width:number,height:number) => {
-  console.log('drawImageToCanvas',img)
+const drawImageToCanvas = (img: HTMLImageElement, width: number, height: number) => {
+  console.log('drawImageToCanvas', img)
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
-  ctx.save() 
+  if (!ctx) return
+  ctx.save()
 
-  canvas.width = width 
+  canvas.width = width
   canvas.height = height
 
   ctx.drawImage(img, 0, 0, width, height)
   ctx.restore()
 
   canvas.toBlob(blob => {
+    if (!blob) {
+      console.error('blob is null')
+      return
+    }
     const url = URL.createObjectURL(blob)
     cropperModel.url = url
 
@@ -194,8 +335,8 @@ const handleMouseOut = () => {
 
 
 
-const getImageData = (img: ImgType): Promise => new Promise(async (resolve, reject) => {
-  console.log(img,'img')
+const getImageData = (img: ImgType) => new Promise(async (resolve, reject) => {
+  console.log(img, 'img')
 
   const obj = {}
 
@@ -233,7 +374,7 @@ const getImageData = (img: ImgType): Promise => new Promise(async (resolve, reje
  * @param {*} reject 
  * @return {*} Promise<Uint8Array>
  */
-const base64ToArrayBuffer = (base64: base64): Promise<Uint8Array> => new Promise((resolve, reject) => {
+const base64ToArrayBuffer = (base64: string): Promise<Uint8Array> => new Promise((resolve, reject) => {
   const binary = atob(base64)
   const array = new Uint8Array(binary.length)
   for (let i = 0; i < binary.length; i++) {
@@ -281,7 +422,7 @@ const blobToArrayBuffer = (blob: Blob): Promise<Uint8Array> => new Promise((reso
  */
 const getOrientation = (arrayBuffer: Uint8Array): Promise<number> => new Promise((resolve, reject) => {
 
-  
+
 })
 
 
@@ -293,26 +434,152 @@ const getOrientation = (arrayBuffer: Uint8Array): Promise<number> => new Promise
 
 <style scoped lang="scss">
 $crop_blue: #16B5FF;
+
+$square_size: 8px;
+$squire_line_size: 1px;
+
+$drop_range: 10px;
+
 .image-cropper {
-  width: 80%;
+  width: 500px;
+  height: 500px;
+  margin: auto;
+  position: relative;
+
   .img-cropper__canvas {
     overflow: hidden;
+    height: 100%;
+    width: 100%;
   }
+
   .img-cropper__cut {
-    position: relative;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    cursor: move;
     outline: 1px solid $crop_blue;
     outline-color: $crop_blue;
-    >li {
-      list-style: none;
-    }
+
+
+
+
     .img-cropper__cut__handle {
-     
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+      >li {
+        list-style: none;
+        list-style-type: none;
+      }
+
+      .square_blue {
+        width: $square_size;
+        height: $square_size;
+        background-color: $crop_blue;
+      }
+      .img-cropper__cut__left_top {
+        position: absolute;
+        left: -$square_size/2;
+        top: -$square_size/2;
+        cursor: nw-resize;
+      }
+      .img-cropper__cut__top {
+        position: absolute;
+        left: 50%;
+        margin-left: -$square_size/2;
+        margin-top: -$square_size/2;
+        cursor: n-resize;
+      }
+      .img-cropper__cut__right_top {
+        position: absolute;
+        right: -$square_size/2;
+        top: -$square_size/2;
+        cursor: ne-resize;
+      }
+      .img-cropper__cut__left {
+        position: absolute;
+        left: -$square_size/2;
+        top: 50%;
+        margin-top: -$square_size/2;
+      }
+      .img-cropper__cut__right {
+        position: absolute;
+        right: -$square_size/2;
+        top: 50%;
+        margin-top: -$square_size/2;
+      }
+      .img-cropper__cut__left_bottom {
+        position: absolute;
+        left: -$square_size/2;
+        bottom: -$square_size/2;
+        cursor: sw-resize;
+      }
+      .img-cropper__cut__bottom {
+        position: absolute;
+        left: 50%;
+        margin-left: -$square_size/2;
+        bottom: -$square_size/2;
+        cursor: s-resize;
+      }
+      .img-cropper__cut__right_bottom {
+        position: absolute;
+        right: -$square_size/2;
+        bottom: -$square_size/2;
+        cursor: se-resize;
+      }
+
+      .img-cropper__cut__line_top {
+        height: $drop_range;
+        position: absolute;
+        width: 100%;
+        top: 0px;
+        left: 0px;
+        cursor: n-resize;
+      }
+      // line
+      .img-cropper__cut__line_top {
+        height: $drop_range;
+        position: absolute;
+        width: 100%;
+        top: 0px;
+        left: 0px;
+        cursor: n-resize;
+      }
+      .img-cropper__cut__line_bottom {
+        height: $drop_range;
+        position: absolute;
+        width: 100%;
+        bottom: 0px;
+        left: 0px;
+        cursor: s-resize;
+      }
+      .img-cropper__cut__line_left {
+        height: 100%;
+        position: absolute;
+        width: $drop_range;
+        top: 0px;
+        left: 0px;
+        cursor: w-resize;
+      }
+      .img-cropper__cut__line_right {
+        height: 100%;
+        position: absolute;
+        width: $drop_range;
+        top: 0px;
+        right: 0px;
+        cursor: e-resize;
+      }
+
+
     }
-    .square_blue {
-      width: 8px;
-      height: 8px;
-      color: $crop_blue;
-    }
+
+
+  }
+  .img-cropper__drop {
+    height: 100%;
+    width: 100%;
   }
 }
 </style>
