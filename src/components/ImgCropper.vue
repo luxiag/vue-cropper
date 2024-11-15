c2e2cf98102b1800cede71b9dc1d22ca54db17d0
luxiag <luxiag@qq.com>
2024-11-15 16:47:58 +0800

    feat: 添加裁剪框方向移动

--- src/components/ImgCropper.vue
+++ src/components/ImgCropper.vue
@@ -4,30 +4,37 @@
     <div class="img-cropper__canvas">
       <img :src="cropperModel.url" alt="" />
     </div>
-    <div class="img-cropper__cut"       :style="{
+    <div class="img-cropper__cut" :style="{
       'width': cutModel.width + 'px',
       'height': cutModel.height + 'px',
-      'transform':`translate(${cutModel.x}px, ${cutModel.y}px)`
+      'transform': `translate(${cutModel.x}px, ${cutModel.y}px)`
 
-    }" 
-
-    > 
-      <div class="img-cropper__cut__move"      @mousedown="handleCutModelMoveDownEvent" 
-      @touchstart="handleCutModelMoveDownEvent"></div>
+    }">
+      <div class="img-cropper__cut__move" @mousedown="handleCutModelMoveDownEvent"
+        @touchstart="handleCutModelMoveDownEvent"></div>
       <ul class="img-cropper__cut__handle">
-        <li class="img-cropper__cut__left_top square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.left_top)"></li>
-        <li class="img-cropper__cut__top square_blue" @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.top)"></li>
-        <li class="img-cropper__cut__right_top square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.right_top)"></li>
-        <li class="img-cropper__cut__left square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.left)"></li>
-        <li class="img-cropper__cut__right square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.right)"></li>
-        <li class="img-cropper__cut__left_bottom square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.left_bottom)"></li>
-        <li class="img-cropper__cut__bottom square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.bottom)"></li>
-        <li class="img-cropper__cut__right_bottom square_blue"  @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.right_bottom)"></li>
+        <li class="img-cropper__cut__left_top square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.left_top)"></li>
+        <li class="img-cropper__cut__top square_blue" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.top)">
+        </li>
+        <li class="img-cropper__cut__right_top square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.right_top)"></li>
+        <li class="img-cropper__cut__left square_blue" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.left)">
+        </li>
+        <li class="img-cropper__cut__right square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.right)"></li>
+        <li class="img-cropper__cut__left_bottom square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.left_bottom)"></li>
+        <li class="img-cropper__cut__bottom square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.bottom)"></li>
+        <li class="img-cropper__cut__right_bottom square_blue"
+          @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.right_bottom)"></li>
 
-        <li class="img-cropper__cut__line_top" @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.top)"></li>
-        <li class="img-cropper__cut__line_right" @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.right)"></li>
-        <li class="img-cropper__cut__line_bottom" @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.bottom)"></li>
-        <li class="img-cropper__cut__line_left" @mousedown="$e =>handleCutModelSizeEvent($e,MoveTypeEnum.left)"></li>
+        <li class="img-cropper__cut__line_top" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.top)"></li>
+        <li class="img-cropper__cut__line_right" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.right)"></li>
+        <li class="img-cropper__cut__line_bottom" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.bottom)">
+        </li>
+        <li class="img-cropper__cut__line_left" @mousedown="$e => handleCutModelSizeEvent($e, MoveTypeEnum.left)"></li>
       </ul>
 
     </div>
@@ -87,20 +94,20 @@ enum MoveTypeEnum {
 const cutModel = reactive({
   maxWidth: 0,
   maxHeight: 0,
-  minWidth:10,
-  minHeight:10,
+  minWidth: 10,
+  minHeight: 10,
   width: 0,
   height: 0,
 
   x: 0,
   y: 0,
   // start move point
-  startMoveX:0,
-  startMoveY:0,
-  beforeMoveX:0,
-  beforeMoveY:0,
-  beforeChangeWidth:0,
-  beforeChangeHeight:0,
+  startMoveX: 0,
+  startMoveY: 0,
+  beforeMoveX: 0,
+  beforeMoveY: 0,
+  beforeChangeWidth: 0,
+  beforeChangeHeight: 0,
   moveType: MoveTypeEnum.left_top
 })
 
@@ -112,13 +119,13 @@ type MouseEventType = MouseEvent | TouchEvent
 const handleCutModelMoveDownEvent = (e: MouseEventType) => {
   e.preventDefault()
   // 双指触控
-  if('touches' in e&&e.touches.length>1){ 
+  if ('touches' in e && e.touches.length > 1) {
     return
   }
 
 
   window.addEventListener('mousemove', moveCutModelEvent)
-  window.addEventListener('mouseup',leaveCutModelEvent)
+  window.addEventListener('mouseup', leaveCutModelEvent)
   window.addEventListener('touchmove', moveCutModelEvent)
   window.addEventListener('touchend', leaveCutModelEvent)
 
@@ -127,26 +134,26 @@ const handleCutModelMoveDownEvent = (e: MouseEventType) => {
 
   cutModel.beforeMoveX = cutModel.x
   cutModel.beforeMoveY = cutModel.y
-  
+
 }
 
 const moveCutModelEvent = (e: MouseEventType) => {
- e.preventDefault()
+  e.preventDefault()
 
   const movingX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
   const movingY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
 
-  const lengthX =movingX - cutModel.startMoveX + cutModel.beforeMoveX
-  const lengthY =movingY - cutModel.startMoveY + cutModel.beforeMoveY
-  nextTick(()=>{
-    cutModel.x = lengthX < 0 ? 0 : lengthX > cutModel.maxWidth - cutModel.width ? cutModel.maxWidth - cutModel.width : lengthX;
-    cutModel.y = lengthY < 0 ? 0 : lengthY > cutModel.maxHeight - cutModel.height ? cutModel.maxHeight - cutModel.height : lengthY;
+  const lengthX = movingX - cutModel.startMoveX + cutModel.beforeMoveX
+  const lengthY = movingY - cutModel.startMoveY + cutModel.beforeMoveY
+  nextTick(() => {
+    cutModel.x = lengthX <= 0 ? 0 : lengthX >= cutModel.maxWidth - cutModel.width ? cutModel.maxWidth - cutModel.width : lengthX;
+    cutModel.y = lengthY <= 0 ? 0 : lengthY >= cutModel.maxHeight - cutModel.height ? cutModel.maxHeight - cutModel.height : lengthY;
   })
 
-  
+
 }
 
-const leaveCutModelEvent = (e:MouseEventType) => {
+const leaveCutModelEvent = (e: MouseEventType) => {
   window.removeEventListener('mousemove', moveCutModelEvent)
   window.removeEventListener('mouseup', leaveCutModelEvent)
   window.removeEventListener('touchmove', moveCutModelEvent)
@@ -154,9 +161,9 @@ const leaveCutModelEvent = (e:MouseEventType) => {
 }
 
 
-const handleCutModelSizeEvent = (e:MouseEventType,type:MoveTypeEnum ) => {
+const handleCutModelSizeEvent = (e: MouseEventType, type: MoveTypeEnum) => {
   e.preventDefault()
-  if('touches' in e&&e.touches.length>1){ 
+  if ('touches' in e && e.touches.length > 1) {
     return
   }
   cutModel.startMoveX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
@@ -165,10 +172,10 @@ const handleCutModelSizeEvent = (e:MouseEventType,type:MoveTypeEnum ) => {
   cutModel.beforeMoveY = cutModel.y
   cutModel.beforeChangeHeight = cutModel.height
   cutModel.beforeChangeWidth = cutModel.width
-  window.addEventListener('mousemove',changeCutModelSize)
-  window.addEventListener('mouseup',leaveCutModelSize)
-  window.addEventListener('touchmove',changeCutModelSize)
-  window.addEventListener('touchend',leaveCutModelSize)
+  window.addEventListener('mousemove', changeCutModelSize)
+  window.addEventListener('mouseup', leaveCutModelSize)
+  window.addEventListener('touchmove', changeCutModelSize)
+  window.addEventListener('touchend', leaveCutModelSize)
   cutModel.moveType = type
 }
 
@@ -177,55 +184,83 @@ const changeCutModelSize = (e: MouseEventType) => {
 
   const movingX = 'clientX' in e ? e.clientX : 'touches' in e ? e.touches[0].clientX : 0
   const movingY = 'clientY' in e ? e.clientY : 'touches' in e ? e.touches[0].clientY : 0
-  const height = cutModel.startMoveY - movingY 
+  const height = cutModel.startMoveY - movingY
+  /*
+    height > 0  : 向上拖动
+   height < 0   : 向下拖动
+   */
   const width = cutModel.startMoveX - movingX
-  switch(cutModel.moveType) { 
+  /*
+    width > 0  : 向左拖动
+    width < 0  : 向右拖动
+  
+  */
+
+  const topSizeChange = () => {
+    if ((cutModel.beforeMoveY - height) >= 0 && height > 0) {
+      cutModel.y = cutModel.beforeMoveY - height
+      cutModel.height = cutModel.beforeChangeHeight + height
+    } else if ((cutModel.beforeChangeHeight + height) >= cutModel.minHeight && height < 0) {
+      cutModel.y = cutModel.beforeMoveY - height
+      cutModel.height = cutModel.beforeChangeHeight + height
+    }
+  }
+  const bottomSizeChange = () => {
+    if ((cutModel.beforeChangeHeight - height) >= cutModel.minHeight && height > 0) {
+      cutModel.height = cutModel.beforeChangeHeight - height
+    } else if (cutModel.beforeChangeHeight - height <= cutModel.maxHeight - cutModel.beforeMoveY && height < 0) {
+      cutModel.height = cutModel.beforeChangeHeight - height
+    }
+  }
+
+  const leftSizeChange = () => {
+    if ((cutModel.beforeMoveX - width) >= 0 && width > 0) {
+      cutModel.x = cutModel.beforeMoveX - width
+      cutModel.width = cutModel.beforeChangeWidth + width
+    } else if ((cutModel.beforeChangeWidth + width) >= cutModel.minWidth && width < 0) {
+      cutModel.x = cutModel.beforeMoveX - width
+      cutModel.width = cutModel.beforeChangeWidth + width
+    }
+  }
+
+  const rightSizeChange = () => {
+    if ((cutModel.beforeChangeWidth - width) >= cutModel.minWidth && width > 0) {
+      cutModel.width = cutModel.beforeChangeWidth - width
+    } else if (cutModel.beforeChangeWidth - width <= cutModel.maxWidth - cutModel.beforeMoveX && width < 0) {
+      cutModel.width = cutModel.beforeChangeWidth - width
+    }
+  }
+  switch (cutModel.moveType) {
     case MoveTypeEnum.top:
-      
-      cutModel.height = 
-        cutModel.beforeChangeHeight + height > cutModel.maxHeight - cutModel.beforeMoveY  ? cutModel.maxHeight - cutModel.beforeMoveY:
-        cutModel.beforeChangeHeight + height < cutModel.minHeight ? cutModel.minHeight : 
-        cutModel.beforeChangeHeight + height
-
-      cutModel.y = cutModel.beforeMoveY - height < 0 ? 0 : 
-        cutModel.beforeMoveY - height > cutModel.maxHeight ? cutModel.maxHeight: 
-        cutModel.beforeMoveY - height    
-    
-      break;
+      topSizeChange()
+      break
     case MoveTypeEnum.bottom:
-      cutModel.height = cutModel.y + cutModel.height >= cutModel.maxHeight ? cutModel.height: 
-        cutModel.beforeChangeHeight - height > cutModel.maxHeight ? cutModel.maxHeight :
-        cutModel.beforeChangeHeight - height < cutModel.minHeight ? cutModel.minHeight : 
-        cutModel.beforeChangeHeight - height
-      break;
+      bottomSizeChange()
+      break
     case MoveTypeEnum.left:
-
-      cutModel.width =cutModel.beforeMoveX - width <= 0 ? cutModel.width:  
-      cutModel.beforeChangeWidth + width > cutModel.maxWidth ? cutModel.maxWidth : 
-      cutModel.beforeChangeWidth + width < cutModel.minWidth ? cutModel.minWidth : 
-      cutModel.beforeChangeWidth + width
-
-      cutModel.x = cutModel.beforeMoveX - width <= 0 ? 0 : 
-      cutModel.beforeMoveX - width > cutModel.maxWidth ? cutModel.maxWidth: 
-      cutModel.beforeMoveX - width
+      leftSizeChange()
       break
     case MoveTypeEnum.right:
-
-      cutModel.width = cutModel.width + cutModel.x >= cutModel.maxWidth ? cutModel.width : 
-        cutModel.beforeChangeWidth - width > cutModel.maxWidth ? cutModel.maxWidth : 
-        cutModel.beforeChangeWidth - width < cutModel.minWidth ? cutModel.minWidth : 
-        cutModel.beforeChangeWidth - width
-
+      rightSizeChange()
       break
     case MoveTypeEnum.left_top:
-      cutModel.width = cutModel.beforeChangeWidth + width > cutModel.maxWidth ? cutModel.maxWidth : cutModel.beforeChangeWidth + width < cutModel.minWidth ? cutModel.minWidth : cutModel.beforeChangeWidth + width
-      cutModel.x = cutModel.beforeMoveX - width < 0 ? 0 : cutModel.beforeMoveX - width > cutModel.maxWidth ? cutModel.maxWidth: cutModel.beforeMoveX - width
-      cutModel.height = cutModel.beforeChangeHeight + height > cutModel.maxHeight ? cutModel.maxHeight : cutModel.beforeChangeHeight + height < cutModel.minHeight ? cutModel.minHeight : cutModel.beforeChangeHeight + height
-      cutModel.y = cutModel.beforeMoveY - height < 0 ? 0 : cutModel.beforeMoveY - height > cutModel.maxHeight ? cutModel.maxHeight: cutModel.beforeMoveY - height
-      break
+      leftSizeChange()
+      topSizeChange()
+      break;
     case MoveTypeEnum.right_top:
+      rightSizeChange()
+      topSizeChange()
     case MoveTypeEnum.left_bottom:
+      leftSizeChange()
+      bottomSizeChange()
+      break;
     case MoveTypeEnum.right_bottom:
+      rightSizeChange()
+      bottomSizeChange()
+      break;
+      default:
+        console.error('未知的方向')
+        break;
 
   }
 
@@ -233,7 +268,7 @@ const changeCutModelSize = (e: MouseEventType) => {
 }
 
 
-const leaveCutModelSize = (e:MouseEventType) => {
+const leaveCutModelSize = (e: MouseEventType) => {
 
   window.removeEventListener('mousemove', changeCutModelSize)
   window.removeEventListener('mouseup', leaveCutModelSize)
@@ -244,16 +279,18 @@ const leaveCutModelSize = (e:MouseEventType) => {
 
 
 const initCutModel = () => {
-  const width = imageCropperRef.value?.offsetWidth || 500
+  //  offsetWidth 包含边框
+  //  clientWidth 不包含边框
+  const width = imageCropperRef.value?.clientWidth || 500
   cutModel.maxWidth = width
   cutModel.maxHeight = width
-  cutModel.width = width -200
-  cutModel.height = width -200
-  cutModel.x = 0
-  cutModel.y = 0
+  cutModel.width = width / 2
+  cutModel.height = width / 2
+  cutModel.x = width / 4
+  cutModel.y = width / 4
 }
 
-onMounted(()=>{
+onMounted(() => {
   initCutModel()
 })
 
@@ -319,7 +356,7 @@ const handleImgChange = () => {
         const URL = window.URL || window.webkitURL
         const src = URL.createObjectURL(blob)
         img.src = src
-      }else {
+      } else {
         console.error('props.img is not File')
       }
 
@@ -494,8 +531,11 @@ $drop_range: 10px;
 .image-cropper {
   width: 500px;
   height: 500px;
-  margin: auto;
+  margin: 50px auto;
   position: relative;
+  border: 1px dashed #ccc;
+  border-radius: 5px;
+  box-sizing: content-box;
 
   .img-cropper__canvas {
     overflow: hidden;
@@ -512,21 +552,24 @@ $drop_range: 10px;
     cursor: move;
     outline: 1px solid $crop_blue;
     outline-color: $crop_blue;
+    box-sizing: border-box;
 
 
 
     .img-cropper__cut__move {
-      width: 100%;
-      height: 100%;
+      width: calc(100% - #{$drop_range});
+      height: calc(100% - #{$drop_range});
       z-index: 1;
       margin: $drop_range/2;
       box-sizing: border-box;
     }
+
     .img-cropper__cut__handle {
       list-style-type: none;
       z-index: 11;
       padding: 0;
       margin: 0;
+
       >li {
         list-style-type: none;
       }
@@ -536,12 +579,14 @@ $drop_range: 10px;
         height: $square_size;
         background-color: $crop_blue;
       }
+
       .img-cropper__cut__left_top {
         position: absolute;
         left: -$square_size/2;
         top: -$square_size/2;
         cursor: nw-resize;
       }
+
       .img-cropper__cut__top {
         position: absolute;
         top: 0;
@@ -550,30 +595,35 @@ $drop_range: 10px;
         margin-top: -$square_size/2;
         cursor: n-resize;
       }
+
       .img-cropper__cut__right_top {
         position: absolute;
         right: -$square_size/2;
         top: -$square_size/2;
         cursor: ne-resize;
       }
+
       .img-cropper__cut__left {
         position: absolute;
         left: -$square_size/2;
         top: 50%;
         margin-top: -$square_size/2;
       }
+
       .img-cropper__cut__right {
         position: absolute;
         right: -$square_size/2;
         top: 50%;
         margin-top: -$square_size/2;
       }
+
       .img-cropper__cut__left_bottom {
         position: absolute;
         left: -$square_size/2;
         bottom: -$square_size/2;
         cursor: sw-resize;
       }
+
       .img-cropper__cut__bottom {
         position: absolute;
         left: 50%;
@@ -581,6 +631,7 @@ $drop_range: 10px;
         bottom: -$square_size/2;
         cursor: s-resize;
       }
+
       .img-cropper__cut__right_bottom {
         position: absolute;
         right: -$square_size/2;
@@ -596,6 +647,7 @@ $drop_range: 10px;
         left: 0px;
         cursor: n-resize;
       }
+
       // line
       .img-cropper__cut__line_top {
         height: $drop_range;
@@ -605,6 +657,7 @@ $drop_range: 10px;
         left: 0px;
         cursor: n-resize;
       }
+
       .img-cropper__cut__line_bottom {
         height: $drop_range;
         position: absolute;
@@ -613,6 +666,7 @@ $drop_range: 10px;
         left: 0px;
         cursor: s-resize;
       }
+
       .img-cropper__cut__line_left {
         height: 100%;
         position: absolute;
@@ -621,6 +675,7 @@ $drop_range: 10px;
         left: 0px;
         cursor: w-resize;
       }
+
       .img-cropper__cut__line_right {
         height: 100%;
         position: absolute;
@@ -635,6 +690,7 @@ $drop_range: 10px;
 
 
   }
+
   .img-cropper__drop {
     height: 100%;
     width: 100%;
